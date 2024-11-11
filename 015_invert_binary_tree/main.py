96e49f2858a2316dd1d93dcaf3e30146e7d6ceca
Gyeha Lim <skywno@gmail.com>
2024-11-11 13:32:17 +0200

    Tree: 015 invert a binary tree

---
+++ 015_invert_binary_tree/main.py
@@ -0,0 +1,65 @@
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+
+    #My answer - depth first, recursive
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+    #Refactor - depth first, recursive
+    def invertTree2(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
+    # breath first using Dequeue
+    def invertTree3(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: 
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return root
+
+
+    def invertTree4(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            return root
+
+        
+    
+    
