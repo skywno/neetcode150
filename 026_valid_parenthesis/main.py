b9679e6d6c55b725239387b7581f1c8df6cbbe3f
Ezra Lim <skywno@gmail.com>
2026-04-03 12:25:18 +0100

    Add stack problem 1

---
+++ 026_valid_parenthesis/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opening_brackets = {'(', '{', '['}
+        arr = []
+        for _ in s: 
+            if _ in opening_brackets:
+                arr.append(_)
+            else
+                
+
