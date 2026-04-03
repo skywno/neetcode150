e11dcd958e863c79af7e4183033553ce36543b42
Ezra Lim <skywno@gmail.com>
2026-04-03 12:25:46 +0100

    Solved stack problem 2 - my own solution

---
+++ 027_min_stack/main.py
@@ -0,0 +1,33 @@
+
+class MinStack:
+
+    def __init__(self):
+        self._min_idx = None
+        self._min_idx_stack = []
+        self._stack = []
+
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+        print(val)
+        if self._min_idx is None or self._stack[self._min_idx] > val:
+            # the latest element added is the minimum value.
+            self._min_idx = len(self._stack) - 1
+            self._min_idx_stack.append(self._min_idx)
+        else:
+            self._min_idx_stack.append(self._min_idx)
+        print(f"min indx: {self._min_idx}")
+
+    def pop(self) -> None:
+        if self._stack:
+            self._min_idx_stack.pop()
+            self._min_idx = self._min_idx_stack[-1]
+            self._stack.pop()
+
+    def top(self) -> int:
+        if self._stack:
+            return self._stack[-1]
+
+    def getMin(self) -> int:
+        print(f"{len(self._stack)} length, but, idx is {self._min_idx}")
+        if self._stack:
+            return  self._stack[self._min_idx]
