613ccd3a76ec4827a3d7b514ceb7f6d9d5629472
Ezra Lim <skywno@gmail.com>
2026-04-03 12:48:24 +0100

    Improve stack problem2

--- 027_min_stack/main.py
+++ 027_min_stack/main.py
@@ -2,36 +2,21 @@
 class MinStack:
 
     def __init__(self):
-        self._min_idx = None
-        self._min_idx_stack = []
+        self._min_stack = []
         self._stack = []
 
     def push(self, val: int) -> None:
         self._stack.append(val)
-        if self._min_idx is None or self._stack[self._min_idx] > val:
-            # the latest element added is the minimum value.
-            self._min_idx = len(self._stack) - 1
-            self._min_idx_stack.append(self._min_idx)
-        else:
-            self._min_idx_stack.append(self._min_idx)
-        print(f"min indx: {self._min_idx}")
+        val = min(val, self._min_stack[-1] if self._min_stack else val)
+        self._min_stack.append(val)
 
     def pop(self) -> None:
-        if self._stack:
-            self._min_idx_stack.pop()
-            self._stack.pop()
-
-            if self._min_idx_stack:
-                self._min_idx = self._min_idx_stack[-1]
-            else:
-                self._min_idx = None
+        self._min_stack.pop()
+        self._stack.pop()
 
     def top(self) -> int:
-        if self._stack:
-            return self._stack[-1]
+        return self._stack[-1]
 
     def getMin(self) -> int:
-        print(f"{len(self._stack)} length, but, idx is {self._min_idx}")
-        if self._stack:
-            return  self._stack[self._min_idx]
+        return  self._min_stack[-1]
 
