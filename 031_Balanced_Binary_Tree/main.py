675c389e149197f7b50139bd3a4bfe39bc42e1d8
Ezra Lim <skywno@gmail.com>
2026-04-11 11:38:45 +0300

    Fix balance tree problems

--- 031_Balanced_Binary_Tree/main.py
+++ 031_Balanced_Binary_Tree/main.py
@@ -10,29 +10,21 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node: TreeNode) -> int:
+        def dfs(node: Optional[TreeNode]) -> int:
             if node is None:
                 print(f"node is None")
-                height = 0
-                return height
-            if node.left is None and node.right is None:
-                print("node left and right is None")
-                print(f"at Node val {node.val}, height is 1")
-                height = 1
-                return height
+                return 0
             
             left_height = dfs(node.left)
+            if left_height == -1:
+                return -1
+            
             right_height = dfs(node.right)
-            print(f"at Node val {node.val}, left_height: {left_height} & right_height: {right_height}")
+            if right_height == -1:
+                return -1
+            
+            if abs(left_height - right_height) > 1:
+                return -1
+
             return max(left_height, right_height) + 1
-        if root is not None:
-            left_depth = dfs(root.left)
-            right_depth = dfs(root.right)
-            print(left_depth)
-            print(right_depth)
-            if abs(left_depth - right_depth) > 1:
-                return False
-            else:
-                return True
-        else:
-            return True
+        return dfs(root) != -1
