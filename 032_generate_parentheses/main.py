1c89e8276dbe4835c07599c386b0a508fd7d48cc
Gyeha Lim <skywno@gmail.com>
2026-04-12 14:44:16 +0300

    Backtracking: generate parenthesis - model solution

--- 032_generate_parentheses/main.py
+++ 032_generate_parentheses/main.py
@@ -19,3 +19,27 @@ class Solution:
         resp = dfs("", 0, 0)
         return resp
 
+	# Model solution with backtracking
+	# Run time: 29ms: Beats 58.09%
+	# Memory: 8.0 MB
+	def generateParenthesis(self, n: int) -> List[str]:
+		stack = []
+		res = []
+
+		def backtrack(open, closed):
+			if open == closed == n:
+				res.append("".join(stack))
+				return
+			
+			if open < n:
+				stack.append("(")
+				bactrack(open +1, closed)
+				stack.pop()
+			if closed < open:
+				stack.append(")")
+				backtrack(open, closed + 1)
+				stack.pop()
+		
+		backtrack(0,0)
+		return res
+			
